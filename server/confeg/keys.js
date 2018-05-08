5f24c3c534adc3d24d4c9a28519f65893ca3d495
falmatham <38674716+falmatham@users.noreply.github.com>
2018-05-08 03:33:40 +0300

    Initial commit

---
+++ server/confeg/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb://admin1:admin1ds027829.mlab.com:27829/sabic18'
+}
---
+++ server/models/post.js
@@ -0,0 +1,54 @@
+const PostSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+
+    },
+    name: {
+        type: String,
+
+    },
+    avatar: {
+        type: String
+
+    },
+
+    likes: [
+        {
+            user: {
+                ref: 'users'
+            }
+        }
+    ],
+
+    comments: [
+        {
+            user: {
+                ref: 'users'
+            },
+            text: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            avatar:{
+                type: String
+            },
+            date: {
+                type: Date
+            },
+
+        }
+    ],
+
+    date: {
+        type: Date
+    },
+
+});
+module.exports= Post = mongoose.model ('users', PostSchema);
